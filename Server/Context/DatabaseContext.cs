96610cb897ef1e6ad4f35573d9fe7d0848ff4fc8
Alolika <Alolika@ALOLIKA>
2021-11-20 10:54:02 +0530

    Add project files.

---
+++ Server/Context/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ABC_Supermarket.Shared.Models;
+
+
+namespace ABC_Supermarket.Server.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        //public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DbSet<ItemDetails> Item { get; set; }
+    }
+}
---
+++ Shared/Models/ItemDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_Supermarket.Shared.Models
+{
+    public class ItemDetails
+    {
+        [Key]
+        public int ItemCode { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string ItemName { get; set; }
+        public double UnitPrice { get; set; }
+        public double Stock { get; set; }
+    }
+}
