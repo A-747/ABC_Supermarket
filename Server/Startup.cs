67250702bce8e18ec4354bf3322eee932f2b63c2
Alolika <Alolika@ALOLIKA>
2021-11-20 10:59:31 +0530

    Adding a comment to initial commit

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -19,7 +19,7 @@ namespace ABC_Supermarket.Server
         }
 
         public IConfiguration Configuration { get; }
-
+        //hola
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
